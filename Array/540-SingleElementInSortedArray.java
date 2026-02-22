31679839e7fc1983c47925e2b9091e21648f12d0
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-02-22 23:25:32 +0530

    Single Element in a Sorted Array

---
+++ Array/540-SingleElementInSortedArray.java
@@ -0,0 +1,58 @@
+//Problem
+
+    /*You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+    
+    Return the single element that appears only once.
+    
+    Your solution must run in O(log n) time and O(1) space.
+    
+     
+    
+    Example 1:
+    
+    Input: nums = [1,1,2,3,3,4,4,8,8]
+    Output: 2
+    Example 2:
+    
+    Input: nums = [3,3,7,7,10,11,11]
+    Output: 10
+     
+    
+    Constraints:
+    
+    1 <= nums.length <= 105
+    0 <= nums[i] <= 105*/
+
+//Solution
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        int low = 0;
+        int high = nums.length -1;
+        if(nums[low] != nums[low +1]) return nums[low];
+        if(nums[high] != nums[high - 1])return nums[high];
+
+        while(low<high){
+            int mid = (low+high)/2;
+
+            if(nums[mid-1] != nums[mid] && nums[mid+1] != nums[mid]){
+                return nums[mid];
+            }
+            if(mid % 2 == 0){
+                if(nums[mid+1] == nums[mid]){
+                    low = mid + 2;
+                }else{
+                    high = mid - 2;
+                }
+            } else {
+                if(nums[mid-1] == nums[mid]){
+                    low = mid + 1;
+                } else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return nums[low];
+    }
+}
