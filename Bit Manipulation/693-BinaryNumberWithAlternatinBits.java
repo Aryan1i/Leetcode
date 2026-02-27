2861d1c56d38a63f7d8b06f1826c263cf2f3b483
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-02-27 22:47:20 +0530

    Binary Number with Alternating Bits
    Implement a solution to check for alternating bits in a positive integer.

---
+++ Bit Manipulation/693-BinaryNumberWithAlternatinBits.java
@@ -0,0 +1,44 @@
+//Problem
+
+    /*Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+    
+     
+    
+    Example 1:
+    
+    Input: n = 5
+    Output: true
+    Explanation: The binary representation of 5 is: 101
+    Example 2:
+    
+    Input: n = 7
+    Output: false
+    Explanation: The binary representation of 7 is: 111.
+    Example 3:
+    
+    Input: n = 11
+    Output: false
+    Explanation: The binary representation of 11 is: 1011.
+     
+    
+    Constraints:
+    
+    1 <= n <= 231 - 1*/
+
+//Solution
+
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int val = n & 1;
+        while(n>0){
+            n>>=1;
+            int temp = n & 1;
+            if(temp == val){
+                return false;
+            }else{
+                val = temp;
+            }
+        }
+        return true;
+    }
+}
