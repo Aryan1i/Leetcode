596176beaf824b6cf324f1920bd8c1b2357bb2e6
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-05-13 22:28:15 +0530

    Minimum Moves to Make Array Complementary
    Implement solution to find minimum moves to make array complementary.

---
+++ Array/1674-MinimumMovesToMakeArrayComplementary.java
@@ -0,0 +1,77 @@
+//Problem
+    
+    /*You are given an integer array nums of even length n and an integer limit. In one move, you can replace any integer from nums with another integer between 1 and limit, inclusive.
+    
+    The array nums is complementary if for all indices i (0-indexed), nums[i] + nums[n - 1 - i] equals the same number. For example, the array [1,2,3,4] is complementary because for all indices i, nums[i] + nums[n - 1 - i] = 5.
+    
+    Return the minimum number of moves required to make nums complementary.
+    
+     
+    
+    Example 1:
+    
+    Input: nums = [1,2,4,3], limit = 4
+    Output: 1
+    Explanation: In 1 move, you can change nums to [1,2,2,3] (underlined elements are changed).
+    nums[0] + nums[3] = 1 + 3 = 4.
+    nums[1] + nums[2] = 2 + 2 = 4.
+    nums[2] + nums[1] = 2 + 2 = 4.
+    nums[3] + nums[0] = 3 + 1 = 4.
+    Therefore, nums[i] + nums[n-1-i] = 4 for every i, so nums is complementary.
+    Example 2:
+    
+    Input: nums = [1,2,2,1], limit = 2
+    Output: 2
+    Explanation: In 2 moves, you can change nums to [2,2,2,2]. You cannot change any number to 3 since 3 > limit.
+    Example 3:
+    
+    Input: nums = [1,2,1,2], limit = 2
+    Output: 0
+    Explanation: nums is already complementary.
+     
+    
+    Constraints:
+    
+    n == nums.length
+    2 <= n <= 105
+    1 <= nums[i] <= limit <= 105
+    n is even.*/
+
+//Solution
+
+class Solution {
+    public int minMoves(int[] nums, int limit) {
+        
+        int n = nums.length;
+
+        int[] diff = new int[2 * limit + 2];
+
+        for(int i = 0; i < n/2; i++) {
+
+            int a = nums[i];
+            int b = nums[n-1-i];
+
+            int minVal = Math.min(a, b) + 1;
+            int maxVal = Math.max(a, b) + limit;
+
+            diff[2] += 2;
+            diff[2*limit+1] -= 2;
+
+            diff[minVal]   += (-1);
+            diff[maxVal+1] -= (-1);
+
+            diff[a+b]   += (-1);
+            diff[a+b+1] -= (-1);
+        }
+
+        int result = Integer.MAX_VALUE;
+
+        for(int sum = 2; sum <= 2*limit; sum++) {
+            diff[sum] += diff[sum-1];
+
+            result = Math.min(result, diff[sum]);
+        }
+
+        return result;
+    }
+}
