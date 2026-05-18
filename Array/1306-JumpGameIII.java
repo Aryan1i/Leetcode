39a4fc5c94188b523b40abed6872e1c0531c74c7
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-05-18 22:49:15 +0530

    Jump Game III
    Implement a solution to determine if it's possible to reach an index with value 0 in the given array using depth-first search.

---
+++ Array/1306-JumpGameIII.java
@@ -0,0 +1,64 @@
+//Problem
+    
+    /*Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
+    
+    Notice that you can not jump outside of the array at any time.
+    
+     
+    
+    Example 1:
+    
+    Input: arr = [4,2,3,0,3,1,2], start = 5
+    Output: true
+    Explanation: 
+    All possible ways to reach at index 3 with value 0 are: 
+    index 5 -> index 4 -> index 1 -> index 3 
+    index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+    Example 2:
+    
+    Input: arr = [4,2,3,0,3,1,2], start = 0
+    Output: true 
+    Explanation: 
+    One possible way to reach at index 3 with value 0 is: 
+    index 0 -> index 4 -> index 1 -> index 3
+    Example 3:
+    
+    Input: arr = [3,0,2,1,2], start = 2
+    Output: false
+    Explanation: There is no way to reach at index 1 with value 0.
+     
+    
+    Constraints:
+    
+    1 <= arr.length <= 5 * 104
+    0 <= arr[i] < arr.length
+    0 <= start < arr.length*/
+
+//Solution
+
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        boolean[] visited = new boolean[arr.length];
+        return dfs(arr, start, visited);
+    }   
+
+    public boolean dfs(int[] arr , int start, boolean[] visited){
+        if(start < 0 || start >= arr.length) return false;
+        if(visited[start] == true) return false;
+
+        visited[start] = true;
+
+        if(arr[start] == 0) return true;
+
+        int left = start - arr[start];
+        int right = start + arr[start];
+
+        boolean l = dfs(arr, left, visited);
+        if(l == true) return true;
+
+        boolean r = dfs(arr, right, visited);
+        if(r == true) return true;
+
+        return false;
+    }
+}
