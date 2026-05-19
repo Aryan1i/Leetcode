1ef2c56f470f8012a2256732c59e46b17d983dae
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-05-19 22:21:58 +0530

    Longest Palindrome

---
+++ String/409-LongestPalindrome.java
@@ -0,0 +1,56 @@
+//Problem
+
+    /*Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+    
+    Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+    
+     
+    
+    Example 1:
+    
+    Input: s = "abccccdd"
+    Output: 7
+    Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+    Example 2:
+    
+    Input: s = "a"
+    Output: 1
+    Explanation: The longest palindrome that can be built is "a", whose length is 1.
+     
+    
+    Constraints:
+    
+    1 <= s.length <= 2000
+    s consists of lowercase and/or uppercase English letters only.*/
+
+//Solution
+
+class Solution {
+    public int longestPalindrome(String s) {
+        
+        int[] fre = new int[52];
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch >= 'a' && ch <= 'z')fre[ch - 'a']++;
+            if(ch >= 'A' && ch <= 'Z')fre[ch - 'A' + 26]++;
+        }
+
+        boolean odd = false;
+        int ans = 0;
+
+        for(int i = 0; i < 52; i++){
+            if(fre[i] % 2 == 0) ans += fre[i];
+            else {
+                odd = true;
+                if(fre[i] > 0){
+                    ans += fre[i] - 1;
+                }
+            }
+        }
+
+        if(odd) ans++;
+
+        return ans;
+    }
+}
