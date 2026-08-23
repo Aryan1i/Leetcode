4b21b40751ff20d6a5c0df94b9b4decfa1307b94
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-08-23 21:50:13 +0530

    Next Greater Node In Linked List

---
+++ Stack/1019. Next Greater Node In Linked List.java
@@ -0,0 +1,75 @@
+//Problem
+    
+    /*You are given the head of a linked list with n nodes.
+    
+    For each node in the list, find the value of the next greater node. That is, for each node, find the value of the first node that is next to it and has a strictly larger value than it.
+    
+    Return an integer array answer where answer[i] is the value of the next greater node of the ith node (1-indexed). If the ith node does not have a next greater node, set answer[i] = 0.
+    
+     
+    
+    Example 1:
+    
+    
+    Input: head = [2,1,5]
+    Output: [5,5,0]
+    Example 2:
+    
+    
+    Input: head = [2,7,4,3,5]
+    Output: [7,0,5,5,0]
+     
+    
+    Constraints:
+    
+    The number of nodes in the list is n.
+    1 <= n <= 104
+    1 <= Node.val <= 109*/
+
+//Solution
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    class Pair{
+        int i;
+        int data;
+        Pair(int i , int data){
+            this.i = i;
+            this.data = data;
+        }
+    }
+    public int[] nextLargerNodes(ListNode head) {
+        Stack<Pair> st = new Stack<>();
+        int n = 0;
+
+        ListNode temp = head;
+        while(temp != null){
+            temp = temp.next;
+            n++;
+        }
+
+        int i = 0;
+        int[] ans = new int[n];
+
+        while(head != null){
+            while(!st.isEmpty() && st.peek().data < head.val){
+                Pair p = st.pop();
+                ans[p.i] = head.val;
+            }
+
+            st.push(new Pair(i++, head.val));
+            head = head.next;
+        }
+
+        return ans;
+    }
+}
