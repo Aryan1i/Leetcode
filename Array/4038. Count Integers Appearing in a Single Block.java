08c3aaebdd3027879ca7f7fe3fea7b17be8aabdc
Aryan Gupta <aryann.gupta.1ii@gmail.com>
2026-08-30 21:25:30 +0530

    Count Integers Appearing in a Single Block

---
+++ Array/4038. Count Integers Appearing in a Single Block.java
@@ -0,0 +1,74 @@
+//Problem
+    
+    /*You are given an integer array nums.
+    
+    An integer x is special if all occurrences of x in nums appear in a single contiguous block.
+    
+    Return the number of distinct special integers in nums.
+    
+     
+    
+    Example 1:
+    
+    Input: nums = [1,2,2,1]
+    
+    Output: 1
+    
+    Explanation:
+    
+    1 appears at indices 0 and 3, forming two separate blocks, so it is not special.
+    2 appears in a single contiguous block at indices [1, 2], so it is special.
+    Therefore, there is one special integer.
+    
+    Example 2:
+    
+    Input: nums = [3,3,1,2,2,1]
+    
+    Output: 2
+    
+    Explanation:
+    
+    3 appears in a single contiguous block at indices [0, 1], so it is special.
+    1 appears at indices 2 and 5, forming two separate blocks, so it is not special.
+    2 appears in a single contiguous block at indices [3, 4], so it is special.
+    Therefore, there are two special integers.
+    
+     
+    
+    Constraints:
+    
+    1 <= nums.length <= 100
+    1 <= nums[i] <= 100*/
+
+//Solution
+
+class Solution {
+    public int countSpecialIntegers(int[] nums) {
+        int n = nums.length;
+        boolean[] b = new boolean[101];
+        Arrays.fill(b, true);
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < n; i++){
+            if(b[nums[i]] == false) continue;
+            int num = nums[i];
+            set.add(num);
+            while(i < n && nums[i] == num) i++;
+            int j = i;
+            i--;
+            while(j < n){
+                if(nums[j++] == num) {
+                    b[num] = false;
+                    break;
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < 101; i++){
+            if(b[i] == true && set.contains(i)) ans++; 
+        }
+        return ans;
+    }
+}
